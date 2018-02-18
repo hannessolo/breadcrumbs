90f8350b61efd67eba770bfb32057e0051c0d770
qzc17 <qzc17@ic.ac.uk>
2018-02-18 06:32:44 +0000

    Added support for types outside of universities

--- backend/controllers/landmarks.js
+++ backend/controllers/landmarks.js
@@ -6,8 +6,48 @@ exports.landmarks = (req, res) => {
   const lat = req.body.lat;
   const long = req.body.lon;
 
-  var types = 'university'
+  var types = 'museum|university|art_gallery';
   var searchString = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+  + lat + ',' + long
+    + '&radius=500&type=' + types + '&key=' + GOOGLE_API_KEY;
+  var r = [];
+  client.get(searchString, '', function (data, response) {
+    var locations = data.results;
+    if (locations.length == 0) {
+      const output = {
+        locName: "There are no landmarks",
+        locations: []
+      }
+      res.send();
+    } else {
+      var address = locations[0].vicinity;
+
+      for (var i = 0; i < locations.length; i++){
+        r.push(
+          {
+            name: locations[i].name,
+            placeID : locations[i].place_id,
+            lat : locations[i].geometry.location.lat,
+            lon : locations[i].geometry.location.lng
+          }
+
+        );
+      }
+      output = {
+        locName: address,
+        locations: r
+      }
+      res.json(output);
+    }
+  });
+};
+
+exports.textLandmarks = (req, res) => {
+  const lat = req.body.lat;
+  const long = req.body.lon;
+
+  var types = 'point_of_interest';
+  var searchString = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?query=Things To See&location='
   + lat + ',' + long
     + '&radius=500&type=' + types + '&key=' + GOOGLE_API_KEY;
   var r = [];
